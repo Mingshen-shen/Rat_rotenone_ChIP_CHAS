d9415b290fcca581ee046a0a72ee07148c4370a5
Mingshen-shen <shenmingshen335@gmail.com>
2025-10-31 17:36:41 +0800

    Delete Run_CHAS directory

--- Run_CHAS/CHAS.R
+++
@@ -1,9 +0,0 @@
-celltype_list <- list(Dopaminergic = da_peaks, Negative = neg_peaks, Neuron = neuron_peaks, Oligodendrocyte = oligodendrocyte_peaks)
-
-celltype_specific_peaks_SN <- CelltypeSpecificPeaks(bulk_SN_df, celltype_list, 0.5)
-
-celltype_scores_SN <- CelltypeScore(counts_mat, celltype_specific_peaks_SN, method="mean") 
-
-plot_celltype_annotations(celltype_specific_peaks_SN)
-
-plot_celltype_scores(celltype_scores_SN, PD_pheno_SN)
