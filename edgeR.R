c89e5e3bd7289f8ad93c9691b77db6a225d54cb1
Mingshen-shen <shenmingshen335@gmail.com>
2025-11-03 11:09:59 +0800

    Update edgeR.R

--- edgeR.R
+++ edgeR.R
@@ -1,33 +1,32 @@
 group_vector <- factor(PD_pheno_SN$Group)
 
+# Constructing the design matrix
+design <- model.matrix(~ group_vector)
+
 # Constructing a DGEList object
 dge <- DGEList(counts = counts_mat)
-
-# Filter out low-expression peaks (optional)
-keep <- filterByExpr(dge, group = group_vector)
-dge <- dge[keep, , keep.lib.sizes = FALSE]
 
 # Standardization
 dge <- calcNormFactors(dge)
 
-# Constructing the design matrix
-design <- model.matrix(~ group_vector)
-
+# Filter out low-expression peaks (optional)
+keep <- filterByExpr(dge, design)
+dge <- dge[keep, , keep.lib.sizes = FALSE]
 
 # Estimated Dispersion
 dge <- estimateDisp(dge, design)
-
-# Fitting Model
-fit <- glmFit(dge, design)
-
-# Perform statistical testing: Test the effect of group_vector (column 2)
-lrt <- glmLRT(fit, coef = 2)
+fit <- glmQLFit(dge, design)
+qlf <- glmQLFTest(fit, coef = 2)  # coef = 2 → GroupRotenone vs PBS
 
 # Extract significantly different peaks (e.g., FDR < 0.05)
-diff_peaks <- topTags(lrt, n = Inf)
-diff_peaks_df <- as.data.frame(diff_peaks)
-sig_peaks <- diff_peaks_df[diff_peaks_df$FDR < 0.05, ]
+dar_results <- topTags(qlf, n = Inf)$table
+
+# Adding label
+dar_results$Status <- ifelse(dar_results$FDR < 0.05 & dar_results$logFC > 0, "Hyper",
+                       ifelse(dar_results$FDR < 0.05 & dar_results$logFC < 0, "Hypo", "NS"))
+
 
 # Further filtering can be performed based on logFC
 hyper_peaks <- rownames(sig_peaks)[sig_peaks$logFC > 0]
 hypo_peaks  <- rownames(sig_peaks)[sig_peaks$logFC < 0]
+
